d38175bfebc40dca3bf4758b1286aaabd4aac73e
Daia Yum <daia.yum@gmail.com>
2015-12-11 16:04:41 +0530

    Add bootstrap codes

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,9 @@
+import org.pf.core.entity.Person;
+
 class BootStrap {
 
     def init = { servletContext ->
+		//new Person(firstName: "Daia", lastName:"Yum", createdBy:"Daia", lastUpdatedBy:"Daia").save();
     }
     def destroy = {
     }
